1fd7d3c4de41c51c91259d3cb978b6ec65a67960
Lewis Dickinson <l.dickinson5382@student.leedsbeckett.ac.uk>
2023-10-18 14:47:53 +0100

    GUI Changes
    Updating GUI to match requirements.
    
    Added:
    - Run Button
    - Syntax Button
    - Open Button
    - Save Button
    - Text Field Display
    - Added Panel for output

--- E-Graphical_Program/E-Graphical program/Form1.cs
+++ E-Graphical_Program/E-Graphical program/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 using Nakov.TurtleGraphics;
 
 namespace E_Graphical_program
@@ -11,32 +14,12 @@ namespace E_Graphical_program
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Turtle.Init(panel4);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Assign a delay to visualize the drawing process
-            Turtle.Delay = 200;
 
-            // Draw a equilateral triangle
-            Turtle.Rotate(30);
-            Turtle.Forward(200);
-            Turtle.Rotate(120);
-            Turtle.Forward(200);
-            Turtle.Rotate(120);
-            Turtle.Forward(200);
-
-            // Draw a line in the triangle
-            Turtle.Rotate(-30);
-            Turtle.PenUp();
-            Turtle.Backward(50);
-            Turtle.PenDown();
-            Turtle.Backward(100);
-            Turtle.PenUp();
-            Turtle.Forward(150);
-            Turtle.PenDown();
-            Turtle.Rotate(30);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -67,6 +50,36 @@ namespace E_Graphical_program
         private void panel4_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Assign a delay to visualize the drawing process
+            Turtle.Delay = 200;
+
+            // Draw a equilateral triangle
+            Turtle.Rotate(30);
+            Turtle.Forward(200);
+            Turtle.Rotate(120);
+            Turtle.Forward(200);
+            Turtle.Rotate(120);
+            Turtle.Forward(200);
+
+            // Draw a line in the triangle
+            Turtle.Rotate(-30);
+            Turtle.PenUp();
+            Turtle.Backward(50);
+            Turtle.PenDown();
+            Turtle.Backward(100);
+            Turtle.PenUp();
+            Turtle.Forward(150);
+            Turtle.PenDown();
+            Turtle.Rotate(30);
         }
     }
 }
