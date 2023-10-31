b80b40e7a6684f2790c863223501565cf3f413f4
Lewis Dickinson <l.dickinson5382@student.leedsbeckett.ac.uk>
2023-10-31 15:26:06 +0000

    Updating GUI
    Restarted with the GUI to meet project requirements going forward.
    
    Added additional buttons, improved layout and better naming structure.

---
+++ E-Graphical Program/E-Graphical Program/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_Graphical_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClickRun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CommandInputTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonClickClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClickSyntax_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonClickOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonClickSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
